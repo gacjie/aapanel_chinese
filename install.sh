4cf475ac8c13d89da9634cdfd3d0d556cf000146
gacjie <90205428+gacjie@users.noreply.github.com>
2022-09-08 21:08:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
-LANG=en_US.UTF-8
-download_Url=https://raw.githubusercontent.com/gacjie/aapanel_chinese/main/panel
-panel_Path=/www/server/panel
-wget -O ${panel_Path}/config/menu.json  ${download_Url}/config/menu.json
-wget -O ${panel_Path}/BTPanel/static/language/English/template.json  ${download_Url}/BTPanel/static/language/English/template.json
-wget -O ${panel_Path}/BTPanel/static/language/English/public.json  ${download_Url}/BTPanel/static/language/English/public.json
-wget -O ${panel_Path}/BTPanel/static/language/English/log.json  ${download_Url}/BTPanel/static/language/English/log.json
-wget -O ${panel_Path}/BTPanel/static/language/English/lan.js  ${download_Url}/BTPanel/static/language/English/lan.js
+wget -O /www/server/panel/config/menu.json https://raw.githubusercontent.com/gacjie/aapanel_chinese/main/panel/config/menu.json -T 10
+wget -O /www/server/panel/BTPanel/static/language/English/template.json https://raw.githubusercontent.com/gacjie/aapanel_chinese/main/panel/BTPanel/static/language/English/template.json -T 10
+wget -O /www/server/panel/BTPanel/static/language/English/public.json https://raw.githubusercontent.com/gacjie/aapanel_chinese/main/panel/BTPanel/static/language/English/public.json -T 10
+wget -O /www/server/panel/BTPanel/static/language/English/log.json https://raw.githubusercontent.com/gacjie/aapanel_chinese/main/panel/BTPanel/static/language/English/log.json -T 10
+wget -O /www/server/panel/BTPanel/static/language/English/lan.js https://raw.githubusercontent.com/gacjie/aapanel_chinese/main/panel/BTPanel/static/language/English/lan.js -T 10
 /etc/init.d/bt restart
