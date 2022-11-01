e7d8112af6bdcce4abca09d18fdd7c53c5606053
gacjie <90205428+gacjie@users.noreply.github.com>
2022-11-01 13:40:32 +0800

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget -O /www/server/panel/config/menu.json https://raw.githubusercontent.com/gacjie/aapanel_chinese/main/panel/config/menu.json -T 10
-wget -O /www/server/panel/BTPanel/static/language/English/template.json https://raw.githubusercontent.com/gacjie/aapanel_chinese/main/panel/BTPanel/static/language/English/template.json -T 10
-wget -O /www/server/panel/BTPanel/static/language/English/public.json https://raw.githubusercontent.com/gacjie/aapanel_chinese/main/panel/BTPanel/static/language/English/public.json -T 10
-wget -O /www/server/panel/BTPanel/static/language/English/log.json https://raw.githubusercontent.com/gacjie/aapanel_chinese/main/panel/BTPanel/static/language/English/log.json -T 10
-wget -O /www/server/panel/BTPanel/static/language/English/lan.js https://raw.githubusercontent.com/gacjie/aapanel_chinese/main/panel/BTPanel/static/language/English/lan.js -T 10
-/etc/init.d/bt restart
